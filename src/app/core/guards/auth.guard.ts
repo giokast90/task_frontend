776127191fdf4ee6b7b173a88bdba48a191b376b
Giovanni Castillo <giovanni@aventontech.com>
2025-06-10 00:54:24 -0600

    "Add documentation for authGuard function in auth.guard.ts"

--- src/app/core/guards/auth.guard.ts
+++ src/app/core/guards/auth.guard.ts
@@ -2,7 +2,17 @@ import { CanActivateFn, Router } from '@angular/router';
 import { inject } from '@angular/core';
 import { AuthService } from "../services/auth/auth.service";
 
-export const authGuard: CanActivateFn = () => {
+/**
+ * Authorization guard function to control route access.
+ *
+ * This function checks if a user is logged in using the `AuthService`.
+ * If the user is logged in, access is granted by returning `true`.
+ * Otherwise, the user is redirected to the '/login' route using the `Router`
+ * and access is denied by returning `false`.
+ *
+ * @returns {boolean} - A boolean indicating whether access is allowed (`true`) or denied (`false`).
+ */
+export const authGuard: CanActivateFn = (): boolean => {
   const auth = inject(AuthService);
   const router = inject(Router);
 
